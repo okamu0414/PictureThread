3279425ffad723c443277c3f5963672d47fbe195
okamu0414 <s.okamuu@icloud.com>
2020-10-30 02:33:00 +0900

    Add AddThread model's column 'thread_image_name'

--- db/migrate/20200227072744_add_thread_image_name_to_add_thread.rb
+++ db/migrate/20200227072744_add_thread_image_name_to_add_thread.rb
@@ -1,5 +1,5 @@
 class AddThreadImageNameToAddThread < ActiveRecord::Migration[5.0]
   def change
-    add_column :Add_Threads, :thread_image_name, :string
+    add_column :add_threads, :thread_image_name, :string
   end
 end
